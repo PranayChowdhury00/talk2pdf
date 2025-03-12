6d4c429f550c5b7cdd99c13aa3f339166acd5b72
PranayChowdhury00 <pranaychowdhury00@gmail.com>
2025-03-13 03:01:23 +0600

    adding header components

---
+++ src/app/components/header.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+
+const Header: FC = () => {
+  return (
+    <header className="  mt-10 mb-10  text-center py-6  ">
+      <h1 className="text-3xl font-bold text-gray-900">PDF to Text Converter</h1>
+      <p className="text-gray-600 mt-2">Get results instantly from your PDF documents</p>
+    </header>
+  );
+};
+
+export default Header;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Header from "@/app/components/header";
 import UploadSection from "@/app/components/upload-section";
 
 export default function Home() {
@@ -7,6 +8,9 @@ export default function Home() {
         <h1 className="text-7xl font-black text-center text-violet-600 pt-24">
           Home Page
         </h1>
+        <div className="">
+          <Header></Header>
+        </div>
         <div className="flex justify-center mt-10 mb-10">
         <UploadSection />
         </div>
