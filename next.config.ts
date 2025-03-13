3263e45ef00c5be61a7f7dca88173bcc6bbbf2ce
PranayChowdhury00 <pranaychowdhury00@gmail.com>
2025-03-13 15:50:49 +0600

    correcting some issue

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "assets.example.com",
+        pathname: "/account123/**",
+      },
+    ],
+    domains: ["randomuser.me"], // ✅ Add this line to allow images from this domain
+  },
 };
 
 export default nextConfig;
